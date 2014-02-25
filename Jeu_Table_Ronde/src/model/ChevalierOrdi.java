d3ff985c2dc0be5b7cd23dae839d4a3f9105f7c3
sarahahm <sarah_ahm@hotmail.fr>
2014-02-25 18:13:18 +0100

    Modif ChevalierOrdi test ok

--- Jeu_Table_Ronde/src/model/ChevalierOrdi.java
+++ Jeu_Table_Ronde/src/model/ChevalierOrdi.java
@@ -2,8 +2,10 @@ package model;
 
 public class ChevalierOrdi extends Chevalier{
 
-	public ChevalierOrdi(String unNom) {
+	public ChevalierOrdi(String unNom){
 		super(unNom);
+	
+		System.out.println("Vous avez créé un chevalier ordi");
 		// TODO Auto-generated constructor stub
 	}
 
