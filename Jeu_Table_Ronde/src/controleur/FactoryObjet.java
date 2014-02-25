beaadf590b76a4dcb84d21d1e478df6248577833
Arthur <a.arthur.robert@gmail.com>
2014-02-25 11:24:03 +0100

    modif création objet obstacle

--- Jeu_Table_Ronde/src/controleur/FactoryObjet.java
+++ Jeu_Table_Ronde/src/controleur/FactoryObjet.java
@@ -6,13 +6,13 @@ public class FactoryObjet {
 	
 	Objet o = null;
 	
-	public static final String[][] DefObjetObstace = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
+	public static final String[][] DefObjetObstacle = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
 	public static final String[][] DefObjetGraal = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
 	
 	public Object createObject(String typeObjet) { 
-		for (int i; i<DefObjet[1].length;i++){	
-			if (typeObjet.equals(DefObjet[1][i])){
-				o = new ObjetObstacle(DefObjet[0][i],Integer.parseInt(DefObjet[2][i]));
+		for (int i; i<DefObjetObstacle.length;i++){	
+			if (typeObjet.equals(DefObjetObstacle[i][0])){
+				o = new ObjetObstacle(DefObjetObstacle[i][0],Integer.parseInt(DefObjetObstacle[i][1]));
 			}
 		}
 		
