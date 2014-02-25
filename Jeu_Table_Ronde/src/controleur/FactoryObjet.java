420081137a1cf53b32b82bf14479f63dfe74340c
Arthur <a.arthur.robert@gmail.com>
2014-02-25 11:17:10 +0100

    ajout création objet obstacle

--- Jeu_Table_Ronde/src/controleur/FactoryObjet.java
+++ Jeu_Table_Ronde/src/controleur/FactoryObjet.java
@@ -4,20 +4,26 @@ import model.*;
 
 public class FactoryObjet {
 	
-	public static final ObjetObstacle[] DefObjet = {"ronce", "puit" ,"falaise", "roche"};
+	Objet o = null;
+	
+	public static final String[][] DefObjetObstace = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
+	public static final String[][] DefObjetGraal = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
 	
 	public Object createObject(String typeObjet) { 
-		if (typeObjet.equals(DefObjet)){
-			Objet o = new ObjetObstacle(typeObjet);
+		for (int i; i<DefObjet[1].length;i++){	
+			if (typeObjet.equals(DefObjet[1][i])){
+				o = new ObjetObstacle(DefObjet[0][i],Integer.parseInt(DefObjet[2][i]));
+			}
 		}
 		
 		else if (typeObjet.equals("graal")){
-			
+			o = new ObjetGraal(DefObjet[0][i],Integer.parseInt(DefObjet[2][i]));
 		}
 
 		else if (typeObjet.equals("chateau")){
 	
 		}
+	}
 
 		return null;
 	}
