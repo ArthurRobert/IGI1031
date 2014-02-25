a6fe312148f2b3a6eb9dfa8c3087ded9a2b500c9
NicoVilm <n.vilmen@live.fr>
2014-02-25 11:32:56 +0100

    DefObjetGraal modif

--- Jeu_Table_Ronde/src/controleur/FactoryObjet.java
+++ Jeu_Table_Ronde/src/controleur/FactoryObjet.java
@@ -6,18 +6,20 @@ public class FactoryObjet {
 	
 	Objet o = null;
 	
-	public static final String[][] DefObjetObstace = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
-	public static final String[][] DefObjetGraal = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
+	public static final String[][] DefObjetObstacle = {{"ronce","-10"},{"puit","-15"},{"falaise","-20"}};
+	public static final String[][] DefObjetGraal = {{"Excalibur","+50"},{"Pierre Fal Lial","+25"},{"Lance de lug","+30"},{"Chaudron Connaissance","+35"}};
 	
 	public Object createObject(String typeObjet) { 
-		for (int i; i<DefObjet[1].length;i++){	
-			if (typeObjet.equals(DefObjet[1][i])){
-				o = new ObjetObstacle(DefObjet[0][i],Integer.parseInt(DefObjet[2][i]));
+		for (int i=0; i<DefObjetObstacle[1].length;i++){	
+			if (typeObjet.equals(DefObjetObstacle[1][i])){
+				o = new ObjetObstacle(DefObjetObstacle[0][i],Integer.parseInt(DefObjetObstacle[2][i]));
 			}
 		}
 		
 		else if (typeObjet.equals("graal")){
-			o = new ObjetGraal(DefObjet[0][i],Integer.parseInt(DefObjet[2][i]));
+			for(int i=0;i<DefObjetGraal[1].length;i++){
+				if (typeObjet.equals(DefObjetGraal[1][i]))
+			o = new ObjetGraal(DefObjetGraal[0][i],Integer.parseInt(DefObjetGraal[3][i]));
 		}
 
 		else if (typeObjet.equals("chateau")){
