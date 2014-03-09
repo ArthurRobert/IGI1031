9dfcd64675bb13fedbd2172cd111922b0ab12d72
BNDevProject <bn.devproject@gmail.com>
2014-03-09 15:05:43 +0100

    creation classe

---
+++ Jeu_Table_Ronde/src/controleur/RatioEcouteur.java
@@ -0,0 +1,21 @@
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Ecouteur pour le choix du ratio, 
+ * 
+ * reste à definir les critères de validité
+ * 
+ * */
+
+public class RatioEcouteur implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Jeu_Table_Ronde/src/controleur/TypeJoueurEcouteur.java
@@ -0,0 +1,19 @@
+package controleur;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Ecouteur le choix du type de joueur
+ * 
+ * */
+
+public class TypeJoueurEcouteur implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
