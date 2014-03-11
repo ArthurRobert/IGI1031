55172e33809a6d0ae465b735836ebcb74fdb0495
BNDevProject <bn.devproject@gmail.com>
2014-03-11 23:32:05 +0100

    test nom de joueur ok, ajout dialog

--- Jeu_Table_Ronde/src/controleur/BoutonCommencerEcouteur.java
+++ Jeu_Table_Ronde/src/controleur/BoutonCommencerEcouteur.java
@@ -1,9 +1,11 @@
 package controleur;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -81,81 +83,82 @@ public class BoutonCommencerEcouteur implements ActionListener {
 		joueursNames.add(joueur2.getText());
 		joueursNames.add(joueur3.getText());
 		joueursNames.add(joueur4.getText());
+		
+		//récupération dimension et ratio
+		int largeur = isDimensionOk(largeurTf.getText());
+		int hauteur = isDimensionOk(hauteurTf.getText());
+		int ratio = ratioTf.getValue();
 
 		//test du nom des joueurs
 		if (isPlayerNameOk(joueursNames))
 		{
 			/**
-			 * Tests du type d'utilisateur
+			 * vérification des dimensions
 			 */
-			//Click on boutonOrdi1
-			if (boutonOrdi1.isSelected()) {
-				//notifier que le joueur 1 sera dirigé par l'ordi
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur1.getText(), "ordi"));
-				System.out.println(joueur1.getText() + " dirigé par l'ordi !");
+			if (hauteur !=-1 && largeur != -1 && ratio != -1) {
+				/**
+				 * Tests du type d'utilisateur
+				 */
+				//Click on boutonOrdi1
+				if (boutonOrdi1.isSelected()) {
+					//notifier que le joueur 1 sera dirigé par l'ordi
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur1.getText(), "ordi"));
+					System.out.println(joueur1.getText() + " dirigé par l'ordi !");
+					
+				}else {
+					//notifier que le joueur 1 sera dirigé par un utilisateur
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur1.getText(), "joueur"));
+					System.out.println(joueur1.getText() + " dirigé par un joueur !");
+				}
 				
-			}else {
-				//notifier que le joueur 1 sera dirigé par un utilisateur
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur1.getText(), "joueur"));
-				System.out.println(joueur1.getText() + " dirigé par un joueur !");
+				//Click on boutonOrdi2
+				if (boutonOrdi2.isSelected()) {
+					//notifier que le joueur 2 sera dirigé par l'ordi
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur2.getText(), "ordi"));
+					System.out.println(joueur2.getText() + " dirigé par l'ordi !");
+				}else {
+					//notifier que le joueur 2 sera dirigé par un utilisateur
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur2.getText(), "joueur"));
+					System.out.println(joueur2.getText() + " dirigé par un joueur !");
+				}
+				
+				//Click on boutonOrdi3
+				if (boutonOrdi3.isSelected()) {
+					//notifier que le joueur 3 sera dirigé par l'ordi
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur3.getText(), "ordi"));
+					System.out.println(joueur3.getText() + " dirigé par l'ordi !");
+				}else {
+					//notifier que le joueur 3 sera dirigé par un utilisateur
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur3.getText(), "joueur"));
+					System.out.println(joueur3.getText() + " dirigé par un joueur !");
+				}
+				
+				//Click on boutonOrdi4
+				if (boutonOrdi4.isSelected()) {
+					//notifier que le joueur 4 sera dirigé par l'ordi
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur4.getText(), "ordi"));
+					System.out.println(joueur4.getText() + " dirigé par l'ordi !");
+				}else {
+					//notifier que le joueur 4 sera dirigé par un utilisateur
+					chevalier_vector.add(FactoryChevalier.addChevalier(joueur4.getText(), "joueur"));
+					System.out.println(joueur4.getText() + " dirigé par un joueur !");
+				}
+				
+				//mettre au dimension
+				((NouvellePartie)o).setMapToFatherPanel(Carte.getInstance(hauteur, largeur, ratio, chevalier_vector));
+				((NouvellePartie)o).dispose();
 			}
-			
-			//Click on boutonOrdi2
-			if (boutonOrdi2.isSelected()) {
-				//notifier que le joueur 2 sera dirigé par l'ordi
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur2.getText(), "ordi"));
-				System.out.println(joueur2.getText() + " dirigé par l'ordi !");
-			}else {
-				//notifier que le joueur 2 sera dirigé par un utilisateur
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur2.getText(), "joueur"));
-				System.out.println(joueur2.getText() + " dirigé par un joueur !");
-			}
-			
-			//Click on boutonOrdi3
-			if (boutonOrdi3.isSelected()) {
-				//notifier que le joueur 3 sera dirigé par l'ordi
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur3.getText(), "ordi"));
-				System.out.println(joueur3.getText() + " dirigé par l'ordi !");
-			}else {
-				//notifier que le joueur 3 sera dirigé par un utilisateur
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur3.getText(), "joueur"));
-				System.out.println(joueur3.getText() + " dirigé par un joueur !");
-			}
-			
-			//Click on boutonOrdi4
-			if (boutonOrdi4.isSelected()) {
-				//notifier que le joueur 4 sera dirigé par l'ordi
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur4.getText(), "ordi"));
-				System.out.println(joueur4.getText() + " dirigé par l'ordi !");
-			}else {
-				//notifier que le joueur 4 sera dirigé par un utilisateur
-				chevalier_vector.add(FactoryChevalier.addChevalier(joueur4.getText(), "joueur"));
-				System.out.println(joueur4.getText() + " dirigé par un joueur !");
+			else {
+				//message d'erreur
+				System.out.println("Erreur de dimension");
+				JOptionPane.showMessageDialog(new Frame(), "Erreur de dimension");
 			}
 		}
 		else 
 		{
 			//message d'erreur
 			System.out.println("Erreur de nom de joueur");
-		}
-		
-		
-		/**
-		 * vérification des dimensions
-		 */
-		//récupération
-		int largeur = isDimensionOk(largeurTf.getText());
-		int hauteur = isDimensionOk(hauteurTf.getText());
-		int ratio = ratioTf.getValue();
-		
-		if (hauteur !=-1 && largeur != -1 && ratio != -1) {
-			//mettre au dimension
-			((NouvellePartie)o).setMapToFatherPanel(Carte.getInstance(hauteur, largeur, ratio, chevalier_vector));
-			((NouvellePartie)o).dispose();
-		}
-		else {
-			//message d'erreur
-			System.out.println("Erreur de dimension");
+			JOptionPane.showMessageDialog(new Frame(), "Erreur de nom de joueur");
 		}
 	}
 	
@@ -164,21 +167,20 @@ public class BoutonCommencerEcouteur implements ActionListener {
 	 * Le nom doit etre rempli et pas plus de 30 caratères
 	 */
 	private boolean isPlayerNameOk(ArrayList<String> joueurs) {
-		
 
 		ArrayList<String> copiListeJoueur = new ArrayList<String>(joueurs);
 		
 		//quelques vérifications de base
 		for (String nomJoueur: joueurs) {
-			if (joueurs.isEmpty() || joueurs==null || joueurs.size()>30) {
+			if (nomJoueur.isEmpty() || nomJoueur==null || nomJoueur.length()>30) {
 				return false;
 			}
+			//les noms doivent être uniques
 			copiListeJoueur.remove(nomJoueur);
 			for (String autreJoueur: copiListeJoueur) {
 				if (nomJoueur.equals(autreJoueur))
 					return false;
 			}
-			//les noms doivent être uniques
 		}
 		// si tout est ok on retourne true
 		return true;
