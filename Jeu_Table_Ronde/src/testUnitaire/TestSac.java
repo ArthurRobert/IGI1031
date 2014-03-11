9dc6496a36c32161161eaa1b8f806747f4dbdd0b
jymaire <jmair.c2i@gmail.com>
2014-03-11 18:27:54 +0100

    Test sac: pas possible, cf code ou document sur Drive

---
+++ Jeu_Table_Ronde/src/testUnitaire/TestSac.java
@@ -0,0 +1,25 @@
+package testUnitaire;
+
+import junit.framework.TestCase;
+import model.ObjetGraal;
+import model.Sac;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSac extends TestCase {
+
+	Sac sac;
+	
+	@Before
+	protected void setUp() throws Exception{
+		super.setUp();
+		// erreur: constructeur en privé donc pas possible de tester
+		//sac = new Sac()
+		
+	}
+	@Test
+	public void testConstructeur(){
+		assertNotNull(sac);
+	}
+}
