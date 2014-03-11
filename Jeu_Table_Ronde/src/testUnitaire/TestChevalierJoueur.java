50ae87bec2f020dab8ff74da9a1b640425391e17
Clim07 <lim.celine@hotmail.fr>
2014-03-11 17:01:29 +0100

    Test objetGraal

--- Jeu_Table_Ronde/src/testUnitaire/TestChevalierJoueur.java
+++ Jeu_Table_Ronde/src/testUnitaire/TestChevalierJoueur.java
@@ -3,6 +3,7 @@ package testUnitaire;
 import junit.framework.TestCase;
 import model.Chevalier;
 import model.ChevalierJoueur;
+import model.ObjetGraal;
 import model.Sac;
 
 import org.junit.Before;
@@ -12,25 +13,27 @@ public class TestChevalierJoueur extends TestCase {
 
 	private Chevalier joueur1;
 	private Sac sac;
+	private ObjetGraal excalibur;
 	
 	@Before
 	protected void setUp() throws Exception{
 		super.setUp();
 		joueur1 = new ChevalierJoueur("Arthur");
 		sac = joueur1.getSacChevalier();
-
-		
+		excalibur = new ObjetGraal("Excalibur", 10, 11);
 	}
 	
+	@Test
 	public void testChevalierJoueur() throws Exception {
 		assertNotNull(joueur1);
-
-
 	}
 		
 	public void testSac(){
 		assertNotNull(sac);
-		
+	}
+	
+	public void testObjet(){
+		assertNotNull(excalibur);
 	}
 
 }
