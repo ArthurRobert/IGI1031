00d944a3d0996eeb263e159e5c762c5e09cc55fc
Marshall71 <marshall.cktm@gmail.com>
2014-03-12 10:57:20 +0100

    Modifcation des structure CartePannel et ClickBoutonEcouteur pour le déplacment du chevalier

--- Jeu_Table_Ronde/src/controleur/ClickBoutonEcouteur.java
+++ Jeu_Table_Ronde/src/controleur/ClickBoutonEcouteur.java
@@ -6,54 +6,35 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 
+import model.Chevalier;
+
 import vue.CartePanel;
 import vue.Case;
 
 public class ClickBoutonEcouteur implements ActionListener{
 
-	ArrayList<ArrayList<Case>> tabLignes = null;
 	CartePanel cartePan;
-	Integer positionClickX, positionClickY;
 
-public ClickBoutonEcouteur(ArrayList<ArrayList<Case>> tabLignes, CartePanel cartePan) {
+public ClickBoutonEcouteur(CartePanel cartePan) {
 		super();
-		this.tabLignes = tabLignes;
 		this.cartePan = cartePan;
 	}
 
-@Override
-public void actionPerformed(ActionEvent e) {
-    Object  source=e.getSource();
-    //On cherche sur quel bouton l'utilisateur a cliqué
-    for(int i=0;i<tabLignes.size();i++){
-    	for(int j=0;j<tabLignes.get(0).size();j++){
-	    	if(source==tabLignes.get(i).get(j)){
-	    		System.out.println(tabLignes.get(i).get(j).getText()); //affichage du numéro de case dans la console
-	    		positionClickX=i;
-	    		positionClickY=j;
-	    	}//fin if
-    	}
-    }
-	
-}
-
-public Integer getPositionX() {
-	return positionClickX;
-}
-
-public void setPositionX(Integer positionX) {
-	this.positionClickX = positionX;
-}
-
-public Integer getPositionY() {
-	return positionClickY;
-}
-
-public void setPositionY(Integer positionY) {
-	this.positionClickY = positionY;
-}
-
-
-
-
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    Object  source=e.getSource();
+	    ArrayList<ArrayList<Case>> tabLignes = cartePan.getTabLignes();
+	    Chevalier chev=cartePan.getChev();
+	    //On cherche sur quel bouton l'utilisateur a cliqué
+	    for(int i=0;i<tabLignes.size();i++){
+	    	for(int j=0;j<tabLignes.get(0).size();j++){
+		    	if(source==tabLignes.get(i).get(j)){
+		    		System.out.println(tabLignes.get(i).get(j).getText()); //affichage du numéro de case dans la console
+		    		chev.setPositionX(i);
+		    		chev.setPositionY(j);
+		    	}//fin if
+	    	}
+	    }
+		
+	}
 }
